ca6d99fa3604b6d4794174e459c44def9b52f061
Valeriia <farewak2@gmail.com>
2023-02-09 20:56:09 +0600

    Removed document

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,19 +1,8 @@
 import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
-import React, { useEffect, useMemo } from 'react';
+import React from 'react';
 import { ModalContainer, ModalContent } from './Modal.styles';
 
 export const Modal = ({ show, children }) => {
-  const el = useMemo(() => document?.createElement('div'), []);
-
-  useEffect(() => {
-    document?.body?.appendChild(el);
-
-    return () => {
-      document?.body?.removeChild(el);
-    };
-  }, []);
-
   return (
     show && (
       <ModalContainer>
