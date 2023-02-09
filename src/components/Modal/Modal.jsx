fc94a76cfb05fca97a9329a5fd74e33bccc32739
Valeriia <farewak2@gmail.com>
2023-02-09 20:47:15 +0600

    Fixes

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,23 +4,21 @@ import React, { useEffect, useMemo } from 'react';
 import { ModalContainer, ModalContent } from './Modal.styles';
 
 export const Modal = ({ show, children }) => {
-  const el = useMemo(() => document.createElement('div'), []);
+  const el = useMemo(() => document?.createElement('div'), []);
 
   useEffect(() => {
-    document.body.appendChild(el);
+    document?.body?.appendChild(el);
 
     return () => {
-      document.body.removeChild(el);
+      document?.body?.removeChild(el);
     };
   }, []);
 
   return (
-    show &&
-    ReactDOM.createPortal(
+    show && (
       <ModalContainer>
         <ModalContent>{children}</ModalContent>
-      </ModalContainer>,
-      el
+      </ModalContainer>
     )
   );
 };
--- src/components/ShippingInfo/ShippingInfo.styles.js
+++ src/components/ShippingInfo/ShippingInfo.styles.js
@@ -3,28 +3,29 @@ import styled from 'styled-components';
 export const ModalHeader = styled.header`
   font-size: 40px;
   font-family: 'Neue Haas Unica Regular Pro';
-  margin-bottom: 15px;
+  margin-bottom: 15px !important;
 `;
 
 export const StyledContent = styled.main`
   li {
-    margin-bottom: 15px;
+    margin-bottom: 15px !important;
 
     h4 {
       font-size: 20px;
       font-family: 'Neue Haas Unica Regular Pro';
-      margin-bottom: 5px;
+      margin-bottom: 5px !important;
     }
   }
 `;
 
 export const ModalFooter = styled.footer`
   display: flex;
-  justify-content: end;
+  justify-content: end !important;
+  padding-top: 0px !important;
 
   button {
     height: 40px;
-    padding-top: 6px;
+    padding-top: 6px !important;
     padding-left: 20px;
     padding-right: 20px;
     font-size: 18px;
